3f492c6b6995db3fe7f86a913486329a067a9e3a
letItBezhumphries <ezhumphries@gmail.com>
2026-07-07 19:16:06 -0700

    added initial terraform code for local development

---
+++ infra/setup/variables.tf
@@ -0,0 +1,19 @@
+variable "tf_state_bucket" {
+  description = "Name of S3 Bucket in AWS for storing TF state"
+  default = "devops-recipe-demoapp-tf-state"
+}
+
+variable "tf_state_lock_table" {
+  description = "Name of DynamoDB Table for TF state locking"
+  default = "devops-recipe-demoapp-api-tf-lock"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default = "recipe-demoapp-api"
+}
+
+variable "contact" {
+  description = "Contact name for tagging resources"
+  default = "ericzackhumphries@gmail.com"
+}
