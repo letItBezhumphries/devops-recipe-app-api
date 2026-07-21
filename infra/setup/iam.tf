270dcfd78dd4ab48fc895fb28f8588455ae266c3
letItBezhumphries <ezhumphries@gmail.com>
2026-07-21 10:40:49 -0700

    added permission to add tags to resource to iam cd user

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -168,6 +168,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
+      "rds:AddTagsToResource",
       "rds:ModifyDBInstance"
     ]
     resources = ["*"]
